250733cc9c4c64d2bce90e1b9ef50dbe45a54fe4
Jae <jykim256@gmail.com>
2022-11-03 03:13:23 -0700

    debug lines

--- reconstruct.py
+++ reconstruct.py
@@ -47,13 +47,14 @@ def reconstruct(fn_img, fn_model, scale, fnhr=None, nbit=16, regular_image=False
             datalr = load_image(fn_img)
             if regular_image:
                 # find maximum and minimum values of datalr
-                print("datalr min", np.min(datalr))
-                print("datalr max", np.max(datalr))
-                print('scale', ((vmaxlr) / (np.max(datalr) - np.min(datalr))))
+                # scale to the range 0 - vmaxlr
+                # print("datalr min", np.min(datalr))
+                # print("datalr max", np.max(datalr))
+                # print('scale', ((vmaxlr) / (np.max(datalr) - np.min(datalr))))
                 datalr = datalr * ((vmaxlr) / (np.max(datalr) - np.min(datalr)))
-                print("datalr min", np.min(datalr))
-                print("datalr max", np.max(datalr))
-            print('datalr shape', datalr.shape)
+                # print("datalr min", np.min(datalr))
+                # print("datalr max", np.max(datalr))
+            # print('datalr shape', datalr.shape)
         except:
             return
 
