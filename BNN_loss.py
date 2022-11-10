24fa26eb1f7d67d3f2bfaef6cefd4ebe409ebe53
jykim256 <jykim256@users.noreply.github.com>
2022-11-10 13:54:52 -0800

    yuh

--- BNN_loss.py
+++ BNN_loss.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 import numpy as np
 
 
-def laplacian_loss(y_pred, y_true):
-    # print(y_true.shape)
-    # print(y_pred.shape)
+def laplacian_loss(y_true, y_pred):
+    print(y_true.shape)
+    print(y_pred.shape)
     mean_true = y_true[:, :, :, 0]
     mean_pred = y_pred[:, :, :, 0]
     scale_pred = y_pred[:, :, :, 1]
