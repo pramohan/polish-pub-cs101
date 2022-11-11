9d85a87590a37cd13d6d3f8e79a6b98b12106230
jykim256 <jykim256@users.noreply.github.com>
2022-11-10 20:54:21 -0800

    yuh

--- model/common.py
+++ model/common.py
@@ -29,8 +29,8 @@ def resolve16(model, lr_batch, nbit=16):
         raise Exception
     lr_batch = tf.stack([lr_batch, lr_batch], axis=3)
     lr_batch = tf.reshape(lr_batch, [lr_batch.shape[0], lr_batch.shape[1], lr_batch.shape[2], 2])
-    print('lrbatchshape')
-    print(lr_batch.shape)
+    # print('lrbatchshape')
+    # print(lr_batch.shape)
     # print('yuh')
     lr_batch = tf.cast(lr_batch, tf.float32)
     # print('aaa')
